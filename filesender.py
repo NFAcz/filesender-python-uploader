d33f9f12dd3b6e84840b5de0c1e4ee8e4f995279
Jonáš Svatoš <jonas.svatos@nfa.cz>
2020-03-27 10:28:59 +0000

    Update filesender.py

--- filesender.py
+++ filesender.py
@@ -55,8 +55,8 @@ parser.add_argument('-r', '--recipients', default=os.getenv('FILESENDER_RECIPIEN
 parser.add_argument('-a', '--apikey', default=os.getenv('FILESENDER_USER_APIKEY', 'TESTAPIKEY'))
 parser.add_argument('-b', '--baseurl', default=os.getenv('FILESENDER_BASEURL', 'localhost'))
 parser.add_argument('-d', '--days', type=int, default=os.getenv('FILESENDER_EXPIRE_DAYS', 10))
-parser.add_argument('-s', '--subject')
-parser.add_argument('-m', '--message')
+parser.add_argument('-s', '--subject', default=os.getenv('FILESENDER_SUBJECT', "NFA Data Transfer"))
+parser.add_argument('-m', '--message', default=os.getenv('FILESENDER_MESSAGE', ""))
 args = parser.parse_args()
 
   
